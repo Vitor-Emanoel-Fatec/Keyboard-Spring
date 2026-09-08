d6c4374f335cfbfa713a5cfb1e9ea1024f7d7f0f
Vitor Emanoel <vitor.silva69@aluno.cps.sp.gov.br>
2026-09-08 14:16:14 -0300

    First commit - Model

---
+++ src/main/java/com/spring_store/SpringStore/models/keyboard.java
@@ -0,0 +1,54 @@
+package com.spring_store.SpringStore.models;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "keyboard")
+public class keyboard {
+    @Id
+    private int id;
+    private String marca;
+    private String modelo;
+    private String cor;
+    private String switch_color;
+
+    public int getId(){
+        return id;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public String getMarca(){
+        return marca;
+    }
+
+    public void setMarca(String marca){
+        this.marca = marca;
+    }
+
+    public String getModelo(){
+        return modelo;
+    }
+
+    public void setModelo(String modelo){
+        this.modelo = modelo;
+    }
+
+    public String getCor(){
+        return cor;
+    }
+
+    public void setCor(String cor){
+        this.cor = cor;
+    }
+
+    public String getSwitch_color(){
+        return switch_color;
+    }
+
+    public void setSwitch_color(String switch_color){
+        this.switch_color = switch_color;
+    }
+}
